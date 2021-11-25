59e0da250cbbf2a24e025b79eb54b8ef059d17a2
Tom <tom.c.maurer@gmail.com>
2021-11-25 16:39:44 -0700

    ridge regression complete

---
+++ src/utility/AnalyzeRegressionResults.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sys
+
+
+def main():
+    fPath = sys.argv[1]
+    scores = np.load(fPath)
+    
+    count = 0
+    for s in scores:
+        if s == 1.0:
+            count += 1
+        
+    print(max(scores), min(scores), np.mean(scores), np.median(scores), count)
+    
+if __name__ == "__main__":
+    main()
